47b9076c534499e0128288249efc97590ffa0a62
Eaglegrant <grantman522@gmail.com>
2023-01-18 23:13:38 -0500

    Broken Fragments

--- app/src/main/java/com/gtappdevelopers/firebasestorageimage/MainActivity.java
+++ app/src/main/java/com/gtappdevelopers/firebasestorageimage/MainActivity.java
@@ -1,273 +1,46 @@
 package com.gtappdevelopers.firebasestorageimage;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.Manifest;
-import android.content.ContentValues;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.Point;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.Display;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.GridView;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.app.lets_go_splash.CreateAnim;
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.zxing.WriterException;
+import com.gtappdevelopers.firebasestorageimage.databinding.ActivityMainBinding;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-
-import androidmads.library.qrgenearator.QRGContents;
-import androidmads.library.qrgenearator.QRGEncoder;
-
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-     EditText dataEdt1;
-    EditText intEdt1;
-    EditText intEdt2;
-    Button generateQrBtn;
-    Bitmap bitmap;
-    QRGEncoder qrgEncoder;
-    String Hang = "Not On Bot";
-    GridView gridView;
-    ArrayList<Integer> gridQR = new ArrayList<Integer>();
-    static final String[] numbers = new String[] {
-            "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜",
-            "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜",
-            "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜",};
-    private boolean checkPermission(){
-        int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if(result == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }else{
-            return false;
-
-        }
-    }
-    private void requestPermission(){
-        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            Toast.makeText(MainActivity.this,"Storage permission is required, please allow it from settings.",Toast.LENGTH_SHORT).show();
-        }else{
-            ActivityCompat.requestPermissions(MainActivity.this,new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},111);
-        }
-    }
-    public void onItemSelected(AdapterView<?> parent, View view,
-                               int pos, long id) {
-         Hang = parent.getItemAtPosition(pos).toString();
-    }
-
-    public void onNothingSelected(AdapterView<?> parent) {
-        Hang = "Not On Bot";
-    }
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        for (int i = 0; i < 27; i++) {
-            gridQR.add(i,0);
-        }
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //initializing all variables.
-        intEdt1 = findViewById(R.id.idInt1);
-        intEdt2 = findViewById(R.id.idInt2);
-        dataEdt1 = findViewById(R.id.idEdt2);
-        Spinner dockSpinner = (Spinner) findViewById(R.id.DockingSpinner);
-        Spinner speedSpinner = (Spinner) findViewById(R.id.SpeedSpinner);
-        Spinner defenseSpinner = (Spinner) findViewById(R.id.DefenseSpinner);
-
-        generateQrBtn = findViewById(R.id.fabGenerate);
-        MaterialButton openQRFolder  = findViewById(R.id.fabFolder);
-        ArrayAdapter<String> DockAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.dockingOrder));
-        DockAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dockSpinner.setAdapter(DockAdapter);
-        dockSpinner.setOnItemSelectedListener(this);
-        ArrayAdapter<String> SpeedAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.DefenseSpeed));
-        SpeedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        speedSpinner.setAdapter(SpeedAdapter);
-        speedSpinner.setOnItemSelectedListener(this);
-        ArrayAdapter<String> DefenseAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.DefenseSpeed));
-        DefenseAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        defenseSpinner.setAdapter(DefenseAdapter);
-        defenseSpinner.setOnItemSelectedListener(this);
-        //intializing onclick listner for button.
-        generateQrBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if ((TextUtils.isEmpty(dataEdt1.getText().toString())) ||
-                        (TextUtils.isEmpty(intEdt1.getText().toString())) || (TextUtils.isEmpty(intEdt2.getText().toString()))){
-                    //if the edittext inputs are empty then execute this method showing a toast message.
-                    Toast.makeText(MainActivity.this, "Enter Values in Everything.", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (checkPermission()) {
-                        //below line is for getting the windowmanager service.
-                        WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
-                        //initializing a variable for default display.
-                        Display display = manager.getDefaultDisplay();
-                        //creating a variable for point which is to be displayed in QR Code.
-                        Point point = new Point();
-                        display.getSize(point);
-                        //getting width and height of a point
-                        int width = point.x;
-                        int height = point.y;
-                        //generating dimension from width and height.
-                        int dimen = width < height ? width : height;
-                        dimen = dimen * 3 / 4;
-                        //setting this dimensions inside our qr code encoder to generate our qr code.
-
-                        String allDataWithLine = intEdt1.getText().toString() + System.lineSeparator() + intEdt2.getText().toString()
-                                + System.lineSeparator() +
-                                dataEdt1.getText().toString() + System.lineSeparator()+ gridQR.toString() + System.lineSeparator() + Hang;
-                        qrgEncoder = new QRGEncoder(allDataWithLine, null, QRGContents.Type.TEXT, dimen);
-                        try {
-                            //getting our qrcode in the form of bitmap.
-                            bitmap = qrgEncoder.encodeAsBitmap();
-                            saveImage(bitmap);
-                            // the bitmap is set inside our image view using .setimagebitmap method.
-                        } catch (WriterException e) {
-                            //this method is called for exception handling.
-                            Log.e("Tag", e.toString());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        Toast.makeText(MainActivity.this, "Complete! When you're done entering QR's, hit Open QR Code Folder", Toast.LENGTH_LONG).show();
-                    }else {
-                        requestPermission();
-                    }
-
-                }
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new BeforeMatchFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.before:
+                    replaceFragment(new BeforeMatchFragment());
+                   break;
+                case R.id.auto:
+                    replaceFragment(new Auto());
+                    break;
+                case R.id.tele:
+                    replaceFragment(new TeleOp());
+                    break;
+                case R.id.end:
+                    replaceFragment(new EndGame());
+                    break;
             }
+            return true;
         });
-        String imagesDir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DCIM).toString() + File.separator + "QR";
-        openQRFolder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (checkPermission()){
-                    Intent intent = new Intent(MainActivity.this,FileListActivity.class);
-                    intent.putExtra("path",imagesDir);
-                    startActivity(intent);
-                }else{
-                    requestPermission();
-                }
-            }
-    });
-        gridView = (GridView) findViewById(R.id.gridView1);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, numbers);
-        gridView.setHorizontalSpacing(2);
-        gridView.setAdapter(adapter);
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                if (getBackgroundColor(v) == Color.parseColor("#FFFFFF")){
-                    v.setBackgroundColor(Color.parseColor("#FF0000"));
-                    gridQR.set(position, 0);
-
-                }else{
-                    v.setBackgroundColor(Color.parseColor("#FFFFFF"));
-                    gridQR.set(position, 1);
-
-                }
-            }
-
-        });
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                    Intent intent = new Intent(MainActivity.this,SplashActivity.class);
-                    startActivity(intent);
-            }
-        });
-    }
-    public static int getBackgroundColor(View view) {
-        Drawable drawable = view.getBackground();
-        if (drawable instanceof ColorDrawable) {
-            ColorDrawable colorDrawable = (ColorDrawable) drawable;
-            if (Build.VERSION.SDK_INT >= 11) {
-                return colorDrawable.getColor();
-            }
-            try {
-                Field field = colorDrawable.getClass().getDeclaredField("mState");
-                field.setAccessible(true);
-                Object object = field.get(colorDrawable);
-                field = object.getClass().getDeclaredField("mUseColor");
-                field.setAccessible(true);
-                return field.getInt(object);
-            } catch (NoSuchFieldException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
         }
-        return 0;
+private void replaceFragment(Fragment fragment) {
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
-    private boolean saveImage(Bitmap bitmap) throws IOException {
-        boolean saved;
-        OutputStream fos=null;
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            ContentValues contentValues =new  ContentValues();
-            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "M" + intEdt2.getText().toString() + " Team " + intEdt1.getText().toString());
-            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/png");
-            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, "DCIM/" + "QR");
-            Uri imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
-            try {
-                fos = getContentResolver().openOutputStream(imageUri);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        } else {
-            String imagesDir = Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_DCIM).toString() + File.separator + "QR";
-
-            File file =new File(imagesDir);
-
-            if (!file.exists()) {
-                file.mkdir();
-            }
-
-            File image =new File(imagesDir,  "M" + intEdt2.getText().toString() + " Team " + intEdt1.getText().toString()+".png");
-            try {
-                fos =new FileOutputStream(image);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-        saved = bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
-        assert fos != null;
-        fos.flush();
-        fos.close();
-        return saved;
-    }
 }
