d5bb29aa017f5d1379796532bf7fbe8f6adc241d
Vazgen Manukyan <vmanukyan135@phm.k12.in.us>
2023-11-01 18:57:00 -0400

    Blue Alliance API

--- app/src/main/java/robotics/scouting/current/BlueAllianceAPI.java
+++ app/src/main/java/robotics/scouting/current/BlueAllianceAPI.java
@@ -12,29 +12,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BlueAllianceAPI {
-    private static RequestQueue requestQueue;
+    private RequestQueue requestQueue; // change to non-static
 
     public BlueAllianceAPI(Context context) {
         requestQueue = Volley.newRequestQueue(context);
     }
 
-    private static final String BASE_URL = "https://www.thebluealliance.com/api/v3";
-    private static final String API_KEY = "0iRhF6zqSQlvSD2VGHX9WS8bIXPVIXU9quISdVV0b0wrD5Z00T2LFSHaxRHLr6et";
+    private final String BASE_URL = "https://www.thebluealliance.com/api/v3";
+    private final String API_KEY = "0iRhF6zqSQlvSD2VGHX9WS8bIXPVIXU9quISdVV0b0wrD5Z00T2LFSHaxRHLr6et";
 
+    // Method for retrieving data from a specific endpoint
     public void getTeamData(String call, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
         String url = BASE_URL + call;
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, listener, errorListener) {
             @Override
             public Map<String, String> getHeaders() {
                 Map<String, String> headers = new HashMap<>();
-                headers.put("X-TBA-Auth-Key", API_KEY);
+                headers.put("X-TBA-Auth-Key", API_KEY); // set the API key in the headers
                 return headers;
             }
         };
-        requestQueue.add(request);
+        requestQueue.add(request); // add the request to the queue
     }
 }
-
-
-
-
