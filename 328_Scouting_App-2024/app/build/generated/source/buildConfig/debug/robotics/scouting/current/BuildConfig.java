2a36e34abef1c50b024d5b5a7c4925b7f2a49513
Aydin42Q <pizzalover42q@gmail.com>
2024-02-28 18:16:11 -0500

    Everything is final except endgame
    Also I don't know whats up with the github, endgame is close to finality

---
+++ 328_Scouting_App-2024/app/build/generated/source/buildConfig/debug/robotics/scouting/current/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package robotics.scouting.current;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.current.files";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 9;
+  public static final String VERSION_NAME = "1.2.0";
+}
